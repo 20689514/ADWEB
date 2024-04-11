2a002ce7ba6b054a64c6616c90afe76913f18dc3
Chrisjohn Crisostomo <119300106+20689514@users.noreply.github.com>
2024-04-11 16:19:29 +0800

    Add files via upload

---
+++ mean/index.js
@@ -0,0 +1,57 @@
+var Express = require('express')
+var Mongoclient = require('mongodb').MongoClient;
+var cors = require('cors');
+const multer = require('multer');
+
+var app = Express();
+app.use(cors());
+
+var CONNECTION_STRING = "mongodb+srv://cjcrisostomo:20689514@cluster0.9jfzzl7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+// database name
+var DATABASENAME = 'MyDb'
+
+// instatiate mongodbclient
+var database;
+
+
+//listener
+app.listen(5038, () => {
+    Mongoclient.connect(CONNECTION_STRING,(error, client) => {database=client.db(DATABASENAME);
+    console.log("Successfully Connected")})
+});
+
+//fetch data  GET
+app.get('/api/books/GetBooks', (req,res) => {
+    database.collection('books').find({}).toArray((error, result) => {
+        res.send(result);
+    })
+})
+
+
+//fetch data POST   
+app.post('/api/books/AddBook', multer().none(), async(req,res) => {
+    try{
+        const numOfDocs = await database.collection("books").countDocuments();
+        await database.collection('books').insertOne({
+            id: (numOfDocs + 1).toString(),
+            title: req.body.title,
+            description: req.body.description,
+            price: req.body.price
+
+        });
+        res.json('Added Succesfully');
+    }catch(error){console.error('Error adding book: ', error);
+    res.status(500).json({error: 'Failed to add book'})
+
+        }
+    }
+)
+
+//DELETE
+app.delete('/api/books/DeleteBook', (req, res) => {
+    database.collection('books').deleteOne({
+        id: req.query.id
+    });
+    res.json('Deleted Successfully');
+})
